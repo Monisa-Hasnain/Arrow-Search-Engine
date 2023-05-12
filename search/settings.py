0de97f84c7ba4ac083577ff39eb2e407772cb0d1
BHariKrishnaReddy <reddyfrn007@gmail.com>
2023-05-12 10:59:24 +0530

    G_API

---
+++ search/settings.py
@@ -0,0 +1,14 @@
+SEARCH_KEY = "AIzaSyCQpJKFLCmXJxu483S0LqS0wQ-3E9J1MJ4"
+SEARCH_ID = "85c95d936b0354434"
+COUNTRY = "us"
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl=" + COUNTRY
+RESULT_COUNT = 20
+
+import os
+# if os.path.exists("private.py"):
+#     from private import *
+
+
+# <script async src="https://cse.google.com/cse.js?cx=85c95d936b0354434">
+# </script>
+# <div class="gcse-search"></div>
