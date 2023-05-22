19a3023dc29b5121dcb5c7673d5218fbb83cf870
B HariKrishna Reddy <45511185+BHariKrishnaReddy@users.noreply.github.com>
2023-05-22 22:07:21 +0530

    Bing

---
+++ Bing/ReadME.md
@@ -0,0 +1,12 @@
+### Implementing Search-Engine with Bing API
+
+```
+├── templates 
+│   ├── index.html
+├── main.py
+├── README.md
+├── search.py
+├── settings.py
+├── storage.py
+├── requirements.txt
+```
---
+++ Bing/settings.py
@@ -0,0 +1,13 @@
+# MySQL database connection details
+MYSQL_HOST = "localhost"
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "root1234"
+MYSQL_DB = "ArrowSearchEngine"
+
+# Bing API parameters
+BING_API_KEY1 = "054614c1743148cf93356f897bb6b50a"
+BING_API_KEY = "ffc01dd7636f4c37b04b4e41bf1a2036"
+BING_ENDPOINT = "https://api.bing.microsoft.com/v7.0/search"
+SEARCH_RESULT_LIMIT = 20
+# BING_ENDPOINT = 'https://api.cognitive.microsoft.com/bing/v7.0/search'
+
