a5db3f5baa066cfe17a2ed012f05945b3dfa4805
B HariKrishna Reddy <45511185+BHariKrishnaReddy@users.noreply.github.com>
2023-05-22 22:09:31 +0530

    ArrowSQL

---
+++ ArrowSE.sql
@@ -0,0 +1,26 @@
+-- Create the database
+CREATE DATABASE ArrowSearchEngine;
+-- drop DATABASE ArrowSearchEngine;
+
+-- Use the database
+USE ArrowSearchEngine;
+
+-- drop database ArrowSearchEngine;
+drop table search_results;
+
+CREATE TABLE search_results (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    query VARCHAR(255),
+    title VARCHAR(255),
+    url VARCHAR(255),
+    snippet TEXT,
+    source VARCHAR(255),
+    image_urls TEXT,
+    video_urls TEXT,
+    priority INT,
+    blocked BOOL
+);
+
+select * from search_results ;
+
+-- select * from results where query='arrow';
